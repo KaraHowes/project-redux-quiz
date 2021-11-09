67ab6ba6d9da2ce5b6e341fb4040c1d14cf88eb0
Kara Howes <karahowes@Karas-MacBook-Air.local>
2021-11-09 22:03:44 +0100

    started styling question component

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import styled from'styled-components'
+
 import { quiz } from '../reducers/quiz'
 
+
+const Buttonssection = styled.section `
+justify-items: centre;
+`
+const Choice = styled.div `
+  display: inline;
+  `
+const Choicebutton = styled.button `
+  width: 25%;
+  margin: 15px;
+  padding: 15px;
+  `
+const Nextbutton = styled.button `
+  width: 30%;
+  margin: 15px;
+  padding: 15px;`
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
   const answers = useSelector((state) => state.quiz.answers)
   const currentQuestionIndex = useSelector((state) => state.quiz.currentQuestionIndex)
   const dispatch = useDispatch()
-  //const correct = useSelector((state) => state.answers.isCorrect)||false 
+  
 
   const nextQuestion = useSelector((state) => state.quiz.goToNextQuestion)
   if (!question) {
@@ -18,29 +36,29 @@ export const CurrentQuestion = () => {
   
   return (
     <div>
-      {answers[currentQuestionIndex] ? answers[currentQuestionIndex].isCorrect : 'no answer'}
-      
-     <button 
-     disabled={!answers[currentQuestionIndex]} 
-     onClick={() => dispatch(quiz.actions.goToNextQuestion())}> Next Question</button>
+      {/*{answers[currentQuestionIndex] ? answers[currentQuestionIndex].isCorrect : 'no answer'}*/}
       <h1>Question: {question.questionText}</h1>
-      {question.options.map((item, index) => (
-        <>
-        <button
-          key={item}
-          onClick={() => onAnswerSubmit(question.id, index)}
-          style={{backgroundColor: !answers ? '#EADCA6': index === question.correctAnswerIndex ? '#17D7A0' : '#B91646'  }}
-        >{item} </button> 
-       
-</>
-      )
-      )}
-
+      <Buttonssection>
+          {question.options.map((item, index) => (
+            <Choice>
+              <Choicebutton
+                key={item}
+                onClick={() => onAnswerSubmit(question.id, index)}
+                style={{backgroundColor: !answers ? '#EADCA6': index === question.correctAnswerIndex ? '#17D7A0' : '#B91646'  }}
+              >{item} </Choicebutton> 
+            </Choice>
+          ))}
+          <Nextbutton 
+          disabled={!answers[currentQuestionIndex]} 
+          onClick={() => dispatch(quiz.actions.goToNextQuestion())}> Next Question</Nextbutton>
+        
+      </Buttonssection>
+      
        <div>
         {answers[currentQuestionIndex]  
           ? answers[currentQuestionIndex].isCorrect 
-            ? <p>It's Correct</p>
-            : <p>Not correct</p>
+            ? <p>Woohoo! It's Correct</p>
+            : <p>Oh no.... It's not correct</p>
           :<p>Waiting for the answer</p>}
         </div>
 

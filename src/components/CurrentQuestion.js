7c3b91432618a152436a322a6303ad3920f84e30
Kara Howes <karahowes@Karas-MacBook-Air.local>
2021-11-09 13:20:02 +0100

    go to next button works

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { quiz } from '../reducers/quiz'
 
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
-  
+  const answers = useSelector((state) => state.quiz.answers)
+  const currentQuestionIndex = useSelector((state) => state.quiz.currentQuestionIndex)
   const dispatch = useDispatch()
 
+  const nextQuestion = useSelector((state) => state.quiz.goToNextQuestion)
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
@@ -15,6 +17,11 @@ export const CurrentQuestion = () => {
   
   return (
     <div>
+      {answers[currentQuestionIndex] ? answers[currentQuestionIndex].isCorrect : 'no answer'}
+
+     <button 
+     disabled={!answers[currentQuestionIndex]} 
+     onClick={() => dispatch(quiz.actions.goToNextQuestion())}> Next Question</button>
       <h1>Question: {question.questionText}</h1>
       {question.options.map((item, index) => (
         <button
@@ -24,4 +31,5 @@ export const CurrentQuestion = () => {
       ))}
     </div>
   )
+ 
 }
